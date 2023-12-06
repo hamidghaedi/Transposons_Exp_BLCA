ebc0b7e5f77ab05663abfd35c34e47cf13028167
Hamid Ghaedi <69734675+hamidghaedi@users.noreply.github.com>
2023-12-06 10:50:42 -0500

    Add files via upload
    A bash script to split a typical sf file (with nrow > 5M, which is a salmon output) into two subsets, zeroExp and nonZeroExp .

---
+++ scripts/spli_sf.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Set up working directory
+cd /home/ghaedi/projects/def-gooding-ab/ghaedi/loci_wise_TE_exp
+
+# Create necessary directories if they don't exist
+mkdir -p nonZero_exp/all
+mkdir -p zero_exp
+
+# Get the total number of .sf files
+total_files=$(ls -1 /home/ghaedi/projects/def-gooding-ab/ghaedi/quant_res_REdisc/*.sf | wc -l)
+file_index=0
+
+# Iterate through all .sf files in quant_res_REdisc
+for sf_file in /home/ghaedi/projects/def-gooding-ab/ghaedi/quant_res_REdisc/*.sf; do
+    ((file_index++))
+
+    # Extract the filename without extension
+    file_basename=$(basename "$sf_file" .sf)
+
+    # Print message indicating the current file being processed
+    echo "Processing file $file_index/$total_files: $sf_file"
+
+    # Perform steps for zero expression
+    echo "Creating zero expression file..."
+    awk '$NF == 0 || NR == 1' "$sf_file" > "zero_exp/${file_basename}_zeroExp.sf"
+
+    # Perform steps for non-zero expression
+    echo "Creating non-zero expression file..."
+    awk '$NF > 0 || NR == 1' "$sf_file" > "nonZero_exp/all/${file_basename}_nonZeroExp.sf"
+
+    # Print a newline for better readability
+    echo
+done
+
+# Print a message indicating the completion of the script
+echo "Processing completed!"
